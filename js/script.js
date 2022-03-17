832e95658246207aa5fd25b23dc5df63260975c3
SamueleDellaRocca <samueledellarocca95@gmail.com>
2022-03-17 16:02:27 +0100

    ricerca

--- js/script.js
+++ js/script.js
@@ -5,12 +5,219 @@ const app = new Vue({
     data: {
 
         oraAttuale: new Date().getHours() + '.' + new Date().getMinutes(),
-
-        ricerca: '',
-        arrayRicercati: [],
-
         nuovoMessaggio: '',
         chatAttiva: 0,
+        ricerca: '',
+
+
+        arrayRicercati: [{
+            nome: 'Michele',
+            avatar: 'avatar_1.jpg',
+            ora: '13.12',
+            arrayMessaggiChat: [
+                {
+                    testo: 'Hai portato a spasso il cane?',
+                    ora: '15.30',
+                    seiIlMittente: true,
+                    visibile: false,
+                },
+                {
+                    testo: 'Ricordati di stendere i panni',
+                    ora: '15.50',
+                    seiIlMittente: true,
+                    visibile: false,
+                },
+                {
+                    testo: 'Tutto fatto!',
+                    ora: '16.15',
+                    seiIlMittente: false,
+                    visibile: false,
+                },
+            ],
+        },
+
+        {
+            nome: 'Fabio',
+            avatar: 'avatar_2.jpg',
+            ora: '13.12',
+            arrayMessaggiChat: [
+                {
+                    testo: 'ciao come stai?',
+                    ora: '15.30',
+                    seiIlMittente: true,
+                    visibile: false,
+                },
+                {
+                    testo: 'io bene, tu che dici?',
+                    ora: '15.50',
+                    seiIlMittente: true,
+                    visibile: false,
+                },
+                {
+                    testo: 'sto scrivendo codice, mi fuma il cervello!',
+                    ora: '17.15',
+                    seiIlMittente: false,
+                    visibile: false,
+                },
+            ],
+        },
+
+        {
+            nome: 'Samuele',
+            avatar: 'avatar_3.jpg',
+            ora: '13.12',
+            arrayMessaggiChat: [
+                {
+                    testo: 'forza roma',
+                    ora: '15.30',
+                    seiIlMittente: true,
+                    visibile: false,
+                },
+                {
+                    testo: 'forza roma',
+                    ora: '15.50',
+                    seiIlMittente: true,
+                    visibile: false,
+                },
+                {
+                    testo: 'sempre!',
+                    ora: '16.30',
+                    seiIlMittente: false,
+                    visibile: false,
+                },
+            ],
+        },
+
+        {
+            nome: 'Alessandro B.',
+            avatar: 'avatar_4.jpg',
+            ora: '13.12',
+            arrayMessaggiChat: [
+                {
+                    testo: 'Hai studiato?',
+                    ora: '15.30',
+                    seiIlMittente: true,
+                    visibile: false,
+                },
+                {
+                    testo: 'lo hai fatto per bene?',
+                    ora: '15.50',
+                    seiIlMittente: true,
+                    visibile: false,
+                },
+                {
+                    testo: 'Tutto fatto!',
+                    ora: '21.25',
+                    seiIlMittente: false,
+                    visibile: false,
+                },
+            ],
+        },
+
+        {
+            nome: 'Alessandro L.',
+            avatar: 'avatar_5.jpg',
+            ora: '13.12',
+            arrayMessaggiChat: [
+                {
+                    testo: 'come va il corso?',
+                    ora: '15.30',
+                    seiIlMittente: true,
+                    visibile: false,
+                },
+                {
+                    testo: 'sei bravo?',
+                    ora: '15.50',
+                    seiIlMittente: true,
+                    visibile: false,
+                },
+                {
+                    testo: 'sono un mostro!',
+                    ora: '16.40',
+                    seiIlMittente: false,
+                    visibile: false,
+                },
+            ],
+        },
+
+        {
+            nome: 'Claudia',
+            avatar: 'avatar_6.jpg',
+            ora: '13.12',
+            arrayMessaggiChat: [
+                {
+                    testo: 'usciamo insieme?',
+                    ora: '15.30',
+                    seiIlMittente: true,
+                    visibile: false,
+                },
+                {
+                    testo: 'mi piaci',
+                    ora: '15.50',
+                    seiIlMittente: true,
+                    visibile: false,
+                },
+                {
+                    testo: 'anche tu!',
+                    ora: '18.15',
+                    seiIlMittente: false,
+                    visibile: false,
+                },
+            ],
+        },
+
+        {
+            nome: 'Federico',
+            avatar: 'avatar_7.jpg',
+            ora: '13.12',
+            arrayMessaggiChat: [
+                {
+                    testo: 'domenica stadio?',
+                    ora: '15.30',
+                    seiIlMittente: true,
+                    visibile: false,
+                },
+                {
+                    testo: 'offro io',
+                    ora: '15.50',
+                    seiIlMittente: true,
+                    visibile: false,
+                },
+                {
+                    testo: 'daje!',
+                    ora: '19.20',
+                    seiIlMittente: false,
+                    visibile: false,
+                },
+            ],
+        },
+
+        {
+            nome: 'Davide',
+            avatar: 'avatar_8.jpg',
+            ora: '13.12',
+            arrayMessaggiChat: [
+                {
+                    testo: 'dopo palestra',
+                    ora: '15.30',
+                    seiIlMittente: true,
+                    visibile: false,
+                },
+                {
+                    testo: 'Petto e bicipidi?',
+                    ora: '15.50',
+                    seiIlMittente: true,
+                    visibile: false,
+                },
+                {
+                    testo: 'forza e coraggio',
+                    ora: '20.12',
+                    seiIlMittente: false,
+                    visibile: false,
+                },
+            ],
+        },
+        ],
 
         arrayContatti: [
 
@@ -230,7 +437,7 @@ const app = new Vue({
 
         funzioneInviaMessaggio() {
             if (this.nuovoMessaggio.trim() != '') {
-                this.arrayContatti[this.chatAttiva].arrayMessaggiChat.push({
+                this.arrayRicercati[this.chatAttiva].arrayMessaggiChat.push({
                     testo: this.nuovoMessaggio,
                     ora: new Date().getHours() + '.' + new Date().getMinutes(),
                     seiIlMittente: true,
@@ -244,7 +451,7 @@ const app = new Vue({
         funzioneRisposta() {
             let ora = new Date().getHours() + '.' + new Date().getMinutes();
 
-            this.arrayContatti[this.chatAttiva].arrayMessaggiChat.push(
+            this.arrayRicercati[this.chatAttiva].arrayMessaggiChat.push(
                 {
                     testo: 'ok',
                     ora: new Date().getHours() + '.' + new Date().getMinutes(),
@@ -268,14 +475,14 @@ const app = new Vue({
         },
 
         funzioneCancellaMessaggio(index) {
-            this.arrayContatti[this.chatAttiva].arrayMessaggiChat.splice(index, 1);
+            this.arrayRicercati[this.chatAttiva].arrayMessaggiChat.splice(index, 1);
         },
 
         funzioneVisibile(index) {
-            if (this.arrayContatti[this.chatAttiva].arrayMessaggiChat[index].visibile == false) {
-                this.arrayContatti[this.chatAttiva].arrayMessaggiChat[index].visibile = true;
+            if (this.arrayRicercati[this.chatAttiva].arrayMessaggiChat[index].visibile == false) {
+                this.arrayRicercati[this.chatAttiva].arrayMessaggiChat[index].visibile = true;
             } else {
-                this.arrayContatti[this.chatAttiva].arrayMessaggiChat[index].visibile = false;
+                this.arrayRicercati[this.chatAttiva].arrayMessaggiChat[index].visibile = false;
             }
         }
 
